981b61dde8ffb713cfea818a20e4582b400ea9ff
AssisDk <50442679+AssisDk@users.noreply.github.com>
2024-02-20 10:43:18 +0100

    opdateret 2.2 program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("hej");
+Console.WriteLine("Hello, World!");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first line of code");
+Console.Write("You wrote your first line of code");
+
+Console.WriteLine("");
+Console.WriteLine(0.25F);
